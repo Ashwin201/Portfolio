54008eb8e2f319d67e14f58995191dae907873d1
Ashwin Sharma <sharmaashwin203@gmail.com>
2024-03-10 22:31:22 +0530

    updated homepage

--- src/components/CVButton.jsx
+++ src/components/CVButton.jsx
@@ -4,7 +4,7 @@ import { RiFolderDownloadFill } from "react-icons/ri";
 import Link from "next/link";
 const CVButton = () => {
   const [data, setData] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -15,8 +15,8 @@ const CVButton = () => {
         });
         if (res.ok) {
           const info = await res.json();
-
           setData(info);
+          setLoading(false);
         }
       } catch (error) {}
     };
@@ -24,26 +24,43 @@ const CVButton = () => {
   }, []);
   return (
     <>
-      {data &&
+      {loading ? (
+        <li
+          className="list-none  mr-3  rounded-md bg-black dark:bg-white text-white dark:text-black  border-solid border-2 border-black
+  dark:border-white  hover:scale-95 transition-all duration-300 ease-in-out max-[300px]:mr-0 max-[300px]:mb-3"
+        >
+          <Link
+            target="_blank"
+            aria-label="Resume"
+            href={`/`}
+            className=" flex items-center    py-[6px] px-3 text-base "
+          >
+            Download CV
+            <span className="ml-2">
+              <RiFolderDownloadFill size={20} className="" />
+            </span>
+          </Link>
+        </li>
+      ) : (
         data?.map((item, index) => (
           <li
-            key={index}
             className="list-none  mr-3  rounded-md bg-black dark:bg-white text-white dark:text-black  border-solid border-2 border-black
-dark:border-white  hover:scale-95 transition-all duration-300 ease-in-out max-[300px]:mr-0 max-[300px]:mb-3"
+    dark:border-white  hover:scale-95 transition-all duration-300 ease-in-out max-[300px]:mr-0 max-[300px]:mb-3"
           >
             <Link
               target="_blank"
               aria-label="Resume"
-              href={item ? `${item?.resume}` : `/`}
+              href={`${item?.resume}`}
               className=" flex items-center    py-[6px] px-3 text-base "
             >
               Download CV
               <span className="ml-2">
-                <RiFolderDownloadFill size={20} className="" />
+                <RiFolderDownloadFill size={20} />
               </span>
             </Link>
           </li>
-        ))}
+        ))
+      )}
     </>
   );
 };
